f8c65d48f8eab9da0d75383e5cb76272e47df96e
banko2 <60343173+banko2@users.noreply.github.com>
2020-01-31 11:04:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesfeet" href="style.css">
+</head>
+<body>
+<header>
+            <h1 style="background-color: red;">Woman Gallery </h1>
+
+      </header>      
+
+<div class="gallery">
+  <a target="_blank" href="ital3.mp4">
+    <img src="ital2.jpg" width="500" height="300">
+  </a>
+  <div class="desc">Woman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.mp4">
+    <img src="ital1.jpg"  width="500" height="300">
+  </a>
+  <div class="desc">Woman1</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bulg.mp4">
+    <img src="bulg.jpg"  width="400" height="300">
+  </a>
+  <div class="desc">Woman2</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.mp4">
+    <img src="usa.jpg"  width="400" height="400">
+  </a>
+  <div class="desc">Woman3</div>
+</div>
+ 
+</body>
+</html>
---
+++ indexwoman.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="gallery">
+  <a target="_blank" href="ital1.jpg">
+    <img src="ital2.jpg" width="800" height="600">
+  </a>
+  <div class="desc">Woman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image1.jpg">
+    <img src="ital1.jpg"  width="800" height="600">
+  </a>
+  <div class="desc">Woman1</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="bulg.mp4">
+    <img src="bulg.jpg"  width="800" height="600">
+  </a>
+  <div class="desc">Woman2</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="image3.jpg">
+    <img src="usa.jpg"  width="800" height="600">
+  </a>
+  <div class="desc">Woman3</div>
+</div>
+
+</body>
+</html>
